536302e730ceb5832f2945e152ff63517ff9cebe
pratheekmadupu <pratheekmadupu2006@gmail.com>
2026-05-22 04:29:49 +0530

    chore: switch Firebase configuration to emergency-qr project

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,14 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1r0rZbS42_JdiPndZXDjsJVoGOWSek9g",
-  authDomain: "savaxa-850c6.firebaseapp.com",
-  projectId: "savaxa-850c6",
-  storageBucket: "savaxa-850c6.firebasestorage.app",
-  messagingSenderId: "883575359557",
-  appId: "1:883575359557:web:b478df5635a352331b3b2c",
-  measurementId: "G-55DPMCJ721"
+  apiKey: "AIzaSyBFXvSwZSrMbYHQkrge6UyZOv2uxU0VkPA",
+  authDomain: "emergency-qr-b0adf.firebaseapp.com",
+  databaseURL: "https://emergency-qr-b0adf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "emergency-qr-b0adf",
+  storageBucket: "emergency-qr-b0adf.firebasestorage.app",
+  messagingSenderId: "326186798135",
+  appId: "1:326186798135:web:21b57be22dff85849303b2",
+  measurementId: "G-SPGK1149TH"
 };
 
 let app, auth, db, storage, analytics;
